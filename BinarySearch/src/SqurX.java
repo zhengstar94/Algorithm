90a5b682475af869fc19ad1100c656f6aab7606a
zxx4 <Aa137988166>
2021-12-13 10:51:05 +0800

    x的平方根，力扣69题

---
+++ BinarySearch/src/SqurX.java
@@ -0,0 +1,58 @@
+/**
+ * 力扣69题
+ *
+ * 给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+ *
+ * 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+ *
+ * 注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
+ *
+ * 示例 1：
+ * 输入：x = 4
+ * 输出：2
+ *
+ * 示例 2：
+ * 输入：x = 8
+ * 输出：2
+ * 解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+ *0 <= x <= 231 - 1
+ **/
+
+/**
+ * @author zxx
+ * @date 2021/12/13 09:46
+ **/
+public class SqurX {
+
+
+
+    public static int mySqrt(int x) {
+        if(x==0 || x==1)
+        {
+            return x;
+        }
+
+        int left = 1;
+        int right = x / 2;
+        while(left < right)
+        {
+            int mid = left + (right - left + 1) / 2;
+            if (mid > x / mid)
+            {
+                right = mid -1;
+            }else
+            {
+                left = mid;
+            }
+        }
+
+        return left;
+
+
+    }
+
+    public static void main(String[] args) {
+        int i = mySqrt(9);
+        System.out.println(i);
+    }
+}
