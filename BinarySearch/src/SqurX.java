472ed4ba9a8d23537148e520edf5bce385add03d
zxx4 <zhengstars@gmail.com>
2022-01-17 16:37:11 +0800

    sqar(x)

--- BinarySearch/src/SqurX.java
+++ BinarySearch/src/SqurX.java
@@ -50,8 +50,34 @@ public class SqurX {
 
     }
 
+    public static int mySqrt2(int x) {
+        if(x==0 || x==1)
+        {
+            return x;
+        }
+
+        int left = 0;
+        int right = x;
+        int ans = -1;
+        // 在区间 [left..right] 查找目标元素
+        while (left <= right) {
+            int mid = left + (right - left ) / 2;
+            // 注意：这里为了避免乘法溢出，改用除法
+            if (mid > x / mid) {
+                // 下一轮搜索区间是 [left..mid - 1]
+                right = mid -1 ;
+            } else {
+                // 下一轮搜索区间是 [mid+1..right]
+                ans = mid;
+                left = mid + 1;
+            }
+        }
+        return ans;
+
+    }
+
     public static void main(String[] args) {
-        int i = mySqrt(9);
+        int i = mySqrt2(9);
         System.out.println(i);
     }
 }
