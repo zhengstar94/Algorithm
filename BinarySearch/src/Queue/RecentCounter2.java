ab24f4b19779de4054dc8fab56433c0e9fd00a41
zxx4 <zhengstars@gmail.com>
2022-01-27 10:56:17 +0800

    933. 最近的请求次数/力扣933

---
+++ BinarySearch/src/Queue/RecentCounter2.java
@@ -0,0 +1,86 @@
+package Queue;
+/**
+ * 933. 最近的请求次数/力扣933
+ *
+ * 写一个 RecentCounter 类来计算特定时间范围内最近的请求。
+ *
+ * 请你实现 RecentCounter 类：
+ *
+ *     RecentCounter() 初始化计数器，请求数为 0 。
+ *     int ping(int t) 在时间 t 添加一个新请求，其中 t 表示以毫秒为单位的某个时间，并返回过去 3000 毫秒内发生的所有请求数（包括新请求）。确切地说，返回在 [t-3000, t] 内发生的请求数。
+ *
+ * 保证 每次对 ping 的调用都使用比之前更大的 t 值。
+ *
+ *
+ *
+ * 示例 1：
+ * 输入：
+ * ["RecentCounter", "ping", "ping", "ping", "ping"]
+ * [[], [1], [100], [3001], [3002]]
+ * 输出：
+ * [null, 1, 2, 3, 3]
+ *
+ * 解释：
+ * RecentCounter recentCounter = new RecentCounter();
+ * recentCounter.ping(1);     // requests = [1]，范围是 [-2999,1]，返回 1
+ * recentCounter.ping(100);   // requests = [1, 100]，范围是 [-2900,100]，返回 2
+ * recentCounter.ping(3001);  // requests = [1, 100, 3001]，范围是 [1,3001]，返回 3
+ * recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002]，范围是 [2,3002]，返回 3
+ *
+ *
+ * 提示：
+ *     1 <= t <= 109
+ *     保证每次对 ping 调用所使用的 t 值都 严格递增
+ *     至多调用 ping 方法 104 次
+ *
+ */
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author zxx
+ * @date 2022/01/26 16:07
+ **/
+public class RecentCounter2 {
+    private ArrayList<Integer> arr;
+    private int left = 0;
+
+    public RecentCounter2() {
+        arr = new ArrayList<>();
+    }
+
+    public int ping(int t) {
+        return bisect(t);
+    }
+
+    private int bisect(int t){
+        arr.add(t);
+        int right = arr.size();
+        while (left < right) {
+            int mid = (right - left) / 2 + left;
+            if (arr.get(mid) < t - 3000){
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return arr.size() - left;
+    }
+
+    public static void main(String[] args) {
+        RecentCounter2 recentCounter = new RecentCounter2();
+        System.out.println(recentCounter.ping(1));
+        System.out.println(recentCounter.ping(100));
+        System.out.println(recentCounter.ping(3001));
+        System.out.println(recentCounter.ping(3002));
+
+        recentCounter.ping(100);
+        recentCounter.ping(3001);
+        recentCounter.ping(3002);
+
+    }
+
+}
