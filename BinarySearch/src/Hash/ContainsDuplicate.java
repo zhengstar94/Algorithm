53bc9fdb5e7be706c1ecb04ddb4ed4c6d9208a0f
zxx4 <zhengstars@gmail.com>
2022-01-30 15:00:35 +0800

    217. 存在重复元素 / 力扣217

---
+++ BinarySearch/src/Hash/ContainsDuplicate.java
@@ -0,0 +1,53 @@
+package Hash;
+/**
+ * 217. 存在重复元素 / 力扣217
+ *
+ * 给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。
+ *
+ * 示例 1：
+ * 输入：nums = [1,2,3,1]
+ * 输出：true
+ *
+ * 示例 2：
+ * 输入：nums = [1,2,3,4]
+ * 输出：false
+ *
+ * 示例 3：
+ * 输入：nums = [1,1,1,3,3,4,3,2,4,2]
+ * 输出：true
+ *
+ *
+ *
+ * 提示：
+ *     1 <= nums.length <= 105
+ *     -109 <= nums[i] <= 109
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author zxx
+ * @date 2022/01/30 14:54
+ **/
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> set = new HashSet<Integer>();
+
+        for (int num : nums)
+        {
+            if(!set.add(num))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
